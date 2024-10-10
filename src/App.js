b842fc42dbfb3b8a4d67b536b051987d4acf22e9
Muskan <muskaniraula@gmail.com>
2024-10-10 11:21:12 +0545

    input with message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const Change = (event) => {
+    setName(event.target.value);
+  };
+  const click = () => {
+    setSubmitted(true);
+  };
+
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <input 
+        type="text" 
+        placeholder="Enter your name" 
+        onChange={Change} 
+      />
+      <button onClick={click}>Submit</button>
+
+      {submitted && name && <h1>Hello {name}!</h1>}
     </div>
-  );
+  ); 
 }
 
 export default App;
